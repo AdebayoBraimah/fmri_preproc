24cdcde9ff99db90ae4ff0c2167547d333b500c6
AdebayoBraimah <adebayo.braimah@gmail.com>
2021-08-22 00:17:23 -0400

    Updated epireg function, added new import statement

--- fmri_preproc/func/registration.py
+++ fmri_preproc/func/registration.py
@@ -22,6 +22,7 @@ from fmri_preproc.utils.tempdir import TmpDir
 from fmri_preproc.utils.util import Command
 from fmri_preproc.utils.logutil import LogFile
 from fmri_preproc.utils.workdir import WorkDir
+from fmri_preproc.utils.enums import BBRType
 
 from fmri_preproc.utils.fileio import (
     File,
@@ -33,7 +34,6 @@ from fmri_preproc.utils.fslpy import (
     fslmaths
 )
 
-
 def fmap_to_struct(outdir: str,
                    fmap: str,
                    fmap_magnitude: str,
@@ -44,7 +44,7 @@ def fmap_to_struct(outdir: str,
                    struct_boundarymask: Optional[str] = None,
                    bbr: bool = False,
                    bbr_slope: float = 0.5,
-                   bbr_type: Optional[str] = None, # This SHOULD be an enum
+                   bbr_type: Optional[str] = "signed",
                    src_space: str = 'native',
                    log: Optional[LogFile] = None
                   ) -> None:
@@ -81,14 +81,14 @@ def fmap_to_struct(outdir: str,
     
     if bbr:
         if (struct_boundarymask is None) or (struct_boundarymask == ""):
-            if log:
-                log.error("RuntimeError: struct_boundarymask is required for BBR")
-            raise RuntimeError('struct_boundarymask is required for BBR')
+            if log: log.error("RuntimeError: 'struct_boundarymask' is required for BBR")
+            raise RuntimeError("'struct_boundarymask' is required for BBR")
 
         if isinstance(bbr_slope, float) or isinstance(bbr_slope, int):
             pass
         else:
-            raise TypeError(f"Input 'bbr_slope': {bbr_slope} is not an 'int' or 'float'.")
+            if log: log.error(f"TypeError: Input 'bbr_slope': {bbr_slope} is not an 'int' nor 'float'.")
+            raise TypeError(f"Input 'bbr_slope': {bbr_slope} is not an 'int' nor 'float'.")
     
     # Define output files
     outputs: Dict[str,str] = {
@@ -121,7 +121,7 @@ def fmap_to_struct(outdir: str,
                                         **kwargs,
                                         'ref_boundarymask': struct_boundarymask,
                                         'bbrslope': bbr_slope,
-                                        'bbrtype': bbr_type,
+                                        'bbrtype': BBRType(bbr_type).name,
                                         'init_affine': outputs.get('init_affine')
                                     }
 
@@ -156,7 +156,80 @@ def struct_to_template_composite():
 def func_to_template_composite():
     pass
 
-def epireg():
+def epireg(src: str,
+           ref: str,
+           src_brainmask: Optional[str] = None,
+           ref_brainmask: Optional[str] = None,
+           src_pedir: Optional[str] = None,
+           src_echospacing: Optional[float] = 0.1,
+           ref_boundarymask: Optional[str] = None,
+           fmap: Optional[str] = None,
+           fmap_brainmask: Optional[str] = None,
+           fmap2ref_xfm: Optional[str] = None,
+           init_xfm: Optional[str] = None,
+           bbrslope: float = -0.5,
+           bbrtype: str = "signed",
+           basename: Optional[str] = None,
+           log: Optional[LogFile] = None,
+           **kwargs
+          ) -> None:
+    """Linear (FLIRT-based) registration of EPI to ref, with optional BBR and distortion correction.
+    """
+    with NiiFile(src=src, assert_exists=True, validate_nifti=True) as sr:
+        with NiiFile(src=ref, assert_exists=True, validate_nifti=True) as rf:
+            src: str = sr.abspath()
+            ref: str = rf.abspath()
+    
+    if log: log.log(f"Performing epireg: Register {src} to {ref} ")
+
+    if basename:
+        pass
+    else: 
+        with NiiFile(src=src) as sr:
+            with NiiFile(src=ref) as rf:
+                dir1, base1, _ = sr.file_parts()
+                _, base2, _ = rf.file_parts()
+                basename: str = os.path.join(dir1,f'{base1}_to_{base2}')
+                dc_warp: str = sr.rm_ext() + '_dc_warp.nii.gz'
+    
+    # Define outputs
+    outputs: Dict[str,str] = {
+                                "affine": f'{basename}_affine.mat',
+                                "init_affine": f'{basename}_init_affine.mat',
+                                "src2ref_init": f'{basename}_init_img.nii.gz',
+                                "warp": f'{basename}_warp.nii.gz',
+                                "inv_affine": f'{basename}_invaffine.mat',
+                                "inv_warp": f'{basename}_invwarp.nii.gz',
+                                "src2ref": f'{basename}_img.nii.gz',
+                                "dc_warp": dc_warp
+                             }
+
+    # Update outputs/templates from kwargs
+    for k,v in kwargs.items():
+        if k not in outputs:
+            raise ValueError(f'Unknown output template {k}')
+        outputs[k] = v
+    
+    # Logic tests
+    _has_fmap = (fmap is not None) and (fmap_brainmask is not None) and (fmap2ref_xfm is not None)
+    _do_bbr = (ref_boundarymask is not None)
+    _do_dc = _has_fmap and (src_pedir is not None) and (src_echospacing is not None)
+
+    if log: log.debug(f'Has fieldmap: {_has_fmap}')
+    if log: log.debug(f'Perform BBR: {_do_bbr}')
+    if log: log.debug(f'Perform distortion correction: {_do_dc}')
+
+    # Check fieldmap inputs
+    if _has_fmap:
+        with NiiFile(src=fmap, assert_exists=True, validate_nifti=True) as fmp:
+            with NiiFile(src=fmap_brainmask, assert_exists=True, validate_nifti=True) as fmb:
+                fmap: str = fmp.abspath()
+                fmap_brainmask: str = fmb.abspath()
+
+    # Phase encode direction
+    # TODO: 
+    #   * Add phase encoding directions dict
+    #   * Add acqp params module
     pass
 
 def nonlinear_reg(src: str,
