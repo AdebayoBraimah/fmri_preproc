c5f9a63a0949c4c01a82973fa71d13209dc17035
AdebayoBraimah <adebayo.braimah@gmail.com>
2021-08-23 14:21:28 -0400

    Added functionality to LogFile class

--- fmri_preproc/utils/logutil.py
+++ fmri_preproc/utils/logutil.py
@@ -2,7 +2,9 @@
 """Logging utility module.
 """
 import logging
+from datetime import datetime
 from fmri_preproc.utils.fileio import File
+from fmri_preproc.utils.enums import LogLevel
 
 class LogFile(File):
     """Convenience class that creates a log file object for logging purposes. Due to how this class is constructed - its 
@@ -25,7 +27,8 @@ class LogFile(File):
     
     def __init__(self,
                  log_file: str = "",
-                 print_to_screen: bool = False
+                 print_to_screen: bool = False,
+                 level: str = "info" # TODO: Need enum here
                 ) -> None:
         """Initialization method for the LogFile class. Initiates logging and its associated methods (from the ``logging`` module).
         
@@ -39,11 +42,17 @@ class LogFile(File):
             print_to_screen: If true, prints output to standard output (stdout) as well.
         """
         self.log_file: str = log_file
+
+        level: str = LogLevel(level.lower()).name
         
-        # Set-up logging to file
-        logging.basicConfig(level=logging.INFO,
+        if level == "info":
+            level: logging.INFO = logging.INFO
+        elif level == "debug":
+            level: logging.DEBUG = logging.DEBUG
+        
+        logging.basicConfig(level=level,
                             format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                            datefmt='%d-%m-%y %H:%M:%S',
+                            datefmt='%m-%d-%y %H:%M:%S',
                             filename=self.log_file,
                             filemode='a')
         
@@ -58,7 +67,8 @@ class LogFile(File):
         super(LogFile, self).__init__(self.log_file)
         
     def info(self,
-             msg: str = "") -> None:
+             msg: str = "",
+             use_header: bool = False) -> None:
         """Writes information to log file.
         
         Usage examples:
@@ -67,11 +77,16 @@ class LogFile(File):
         
         Arguments:
             msg: String to be printed to log file.
+            use_header: Give log message a section header.
         """
-        self.logger.info(msg)
+        if use_header:
+            self.logger.info(self._section_header(msg))
+        else:
+            self.logger.info(msg)
         
     def debug(self,
-              msg: str = "") -> None:
+              msg: str = "",
+              use_header: bool = False) -> None:
         """Writes debug information to file.
         
         Usage examples:
@@ -80,11 +95,34 @@ class LogFile(File):
         
         Arguments:
             msg: String to be printed to log file.
+            use_header: Give log message a section header.
         """
-        self.logger.debug(msg)
+        if use_header:
+            self.logger.debug(self._section_header(msg))
+        else:
+            self.logger.debug(msg)
+    
+    def critical(self,
+                 msg: str,
+                 use_header: bool = False) -> None:
+        """Write critical messages/information to file.
+
+        Usage example:
+            >>> log = LogFile("file.log")
+            >>> log.critical("<str>")
+
+        Arguments:
+            msg: String to be printed to log file.
+            use_header: Give log message a section header. 
+        """
+        if use_header:
+            self.logger.critical(self._section_header(msg))
+        else:
+            self.logger.critical(msg)
         
     def error(self,
-              msg: str = "") -> None:
+              msg: str = "",
+              use_header: bool = False) -> None:
         """Writes error information to file.
         
         Usage examples:
@@ -93,11 +131,16 @@ class LogFile(File):
         
         Arguments:
             msg: String to be printed to log file.
+            use_header: Give log message a section header.
         """
-        self.logger.error(msg)
+        if use_header:
+            self.logger.error(self._section_header(msg))
+        else:
+            self.logger.error(msg)
         
     def warning(self,
-                msg: str = "") -> None:
+                msg: str = "",
+                use_header: bool = False) -> None:
         """Writes warnings to file.
         
         Usage examples:
@@ -106,11 +149,16 @@ class LogFile(File):
         
         Arguments:
             msg: String to be printed to log file.
+            use_header: Give log message a section header.
         """
-        self.logger.warning(msg)
+        if use_header:
+            self.logger.warning(self._section_header(msg))
+        else:
+            self.logger.warning(msg)
     
     def log(self,
-            log_cmd: str = "") -> None:
+            log_cmd: str = "",
+            use_header: bool = False) -> None:
         """Log function for logging commands and messages to some log file.
         
         Usage examples:
@@ -118,6 +166,35 @@ class LogFile(File):
             >>> log.log("<str>")
             
         Arguments:
-            log_cmd: Message to be written to log file
+            log_cmd: Message to be written to log file.
+            use_header: Give log message a section header.
         """
-        self.info(log_cmd)
+        if use_header:
+            self.info(self._section_header(log_cmd))
+        else:
+            self.info(log_cmd)
+    
+    def _section_header(self,
+                        msg: str) -> str:
+        """Helper function that adds a section header that consists of
+        a line break, with the date, time, and message string.
+
+        Usage example:
+            >>> _section_header("INFO: This is a test")
+
+            -------------------------------------------------------------
+            Mon Aug 23 13:34:21 2021 : INFO: This is a test
+            -------------------------------------------------------------
+
+        Arguments:
+            msg: Message string to have section header.
+
+        Returns:
+            String that represents the message with header.
+        """
+        header: str = f"""\n
+-------------------------------------------------------------
+{datetime.now().ctime()} : {msg}
+-------------------------------------------------------------
+        """
+        return header
