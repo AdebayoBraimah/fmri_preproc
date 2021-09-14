f27138ddaee8dc04ef971065d3e6962bf93d64bb
AdebayoBraimah <adebayo.braimah@gmail.com>
2021-09-14 03:33:20 -0400

    Added CLI

--- fmri_preproc/fmri_preproc.py
+++ fmri_preproc/fmri_preproc.py
@@ -4,7 +4,6 @@
 """
 # TODO: 
 #   * Write CLI
-#   * Write function for multi-atlas registration
 #   * Create settings function(s)/module and corresponding settings(.json) file.
 
 import os
@@ -15,13 +14,16 @@ from typing import (
     Tuple
 )
 
-from fmri_preproc.func.pipeline import Pipeline
+# from fmri_preproc.func.pipeline import Pipeline
 
 
-def main():
+def main() -> int:
     """Main function.
     """
-    pass
+    
+    args, parser = arg_parser()
+
+    return 0
 
 
 def preproc_data():
@@ -37,7 +39,7 @@ def arg_parser() -> Tuple[argparse.ArgumentParser.parse_args, argparse.ArgumentP
     """Argument parser.
     """
     # Init parser
-    parser: argparse.ArgumentParser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description=__doc__)#, formatter_class=argparse.RawTextHelpFormatter)
 
     # Parse Arguments
 
@@ -159,6 +161,123 @@ def arg_parser() -> Tuple[argparse.ArgumentParser.parse_args, argparse.ArgumentP
                                metavar="<float>",
                                dest="spinecho_inplaneacc",
                                help="Spinecho MR image inplane acceleration . If not specified, then it is assumed to match '--func-inplane-accel'.")
+    importoptions.add_argument('--sp-AP',
+                               type=str,
+                               metavar="<file>",
+                               dest="ap_dir",
+                               help="A -> P (anterior to posterior) Spinecho MR image data.")
+    importoptions.add_argument('--sp-PA',
+                               type=str,
+                               metavar="<file>",
+                               dest="pa_dir",
+                               help="P -> A (posterior to anterior) Spinecho MR image data.")
+    importoptions.add_argument('--sp-LR',
+                               type=str,
+                               metavar="<file>",
+                               dest="lr_dir",
+                               help="L -> R (left to right) Spinecho MR image data.")
+    importoptions.add_argument('--sp-RL',
+                               type=str,
+                               metavar="<file>",
+                               dest="rl_dir",
+                               help="R -> L (right to left) Spinecho MR image data.")
+    importoptions.add_argument('--sp-IS',
+                               type=str,
+                               metavar="<file>",
+                               dest="is_dir",
+                               help="I -> S (inferior to superior) Spinecho MR image data.")
+    importoptions.add_argument('--sp-SI',
+                               type=str,
+                               metavar="<file>",
+                               dest="si_dir",
+                               help="S -> I (superior to inferior) Spinecho MR image data.")
+
+    mcdcoptions = parser.add_argument_group('Motion Correction and Distortion Correction Arguments')
+
+    mcdcoptions.add_argument('--s2v', '--slice-to-volume',
+                             dest="s2v",
+                             action="store_true",
+                             help="Enables slice-to-volume motion correction. NOTE: This will LIKELY require EDDY_CUDA and GPU processing. This is possible without GPUs in FSL v6.0.5+ but is VERY SLOW.")
+    mcdcoptions.add_argument('--dc', '--distortion-correction',
+                             dest="dc",
+                             action="store_true",
+                             help="Enables distortion correction.")
+    mcdcoptions.add_argument('--mbs',
+                             dest="mbs",
+                             action="store_true",
+                             help="Enables movement by susceptibility distortion correction.")
+    mcdcoptions.add_argument('--mcflirt',
+                             dest="use_mcflirt",
+                             action="store_true",
+                             help="Enables FSL's MCFLIRT-based motion correction. NOTE: Enabling this option DISABLES: '--s2v', '--dc', and '--mbs' options.")
+
+    regoptions = parser.add_argument_group('Template Registration Arguments')
+
+    regoptions.add_argument('--standard-age',
+                            type=int,
+                            metavar="<int>",
+                            dest="standard_age",
+                            help="Standard age to use for final dHCP age-matched template (PMA, in weeks) [default=40 (wks)].")
+    regoptions.add_argument('--template-ages',
+                            type=str,
+                            metavar="<str,str,..str>",
+                            dest="template_ages",
+                            help="Comma separated list of ages. Acceptable inputs include: 'neo', 1, 2 (yrs), and/or 28 - 44 (PMA, wks).")
+    regoptions.add_argument('--atlas-dir',
+                            type=str,
+                            metavar="<path>",
+                            dest="atlasdir",
+                            help="Atlas directory path.")
+
+    icaoptions = parser.add_argument_group('ICA Arguments')
+
+    icaoptions.add_argument('--temporal-fwhm',
+                            type=float,
+                            metavar="<float>",
+                            dest="temporal_fwhm",
+                            help="Temporal FWHM filtering coefficient [default=150.0 (s)].")
+    icaoptions.add_argument('--ica-dim',
+                            type=int,
+                            metavar="<int>",
+                            dest="icadim",
+                            help="Maximal ICA dimensionality.")
+
+    fixoptions = parser.add_argument_group('Denoise/FIX Arguments')
+
+    fixoptions.add_argument('--rdata',
+                            type=str,
+                            metavar="<str>",
+                            dest="rdata",
+                            help="Trained FIX classifier (stored as a Rdata file).")
+    fixoptions.add_argument('--fix-threshold',
+                            type=str,
+                            metavar="<str>",
+                            dest="rdata",
+                            help="FIX noise classification threshold. [default= TODO: SET LATER].")
+
+    pipeoptions = parser.add_argument_group('Pipeline Runtime Behavior Arguments')
+
+    pipeoptions.add_argument('--pre-mcdc',
+                             dest="pre_mcdc",
+                             action="store_true",
+                             help="Perform pipeline preprocessing steps prior to motion correction and distortion correction.")
+    pipeoptions.add_argument('--mcdc',
+                             dest="mcdc",
+                             action="store_true",
+                             help="Perform motion correction and distortion correction step exclusively.")
+    pipeoptions.add_argument('--post-mcdc',
+                             dest="post_mcdc",
+                             action="store_true",
+                             help="Perform pipeline preprocessing steps after motion correction and distortion correction.")
+
+    optoptions = parser.add_argument_group('Optional Arguments')
+
+    optoptions.add_argument('--config', '--settings',
+                            type=str,
+                            metavar="<file>",
+                            dest="settings",
+                            help="Configuration (JSON) file that contains additional input arguments not specified at the command line.")
+
 
     args: argparse.ArgumentParser.parse_args = parser.parse_args()
 
